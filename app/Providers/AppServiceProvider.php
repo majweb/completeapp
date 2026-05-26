46951aa9aa99640b199b329e2a5677ad01a3cb05
marcin <marcin.five@gmail.com>
2026-05-26 09:50:27 +0200

    clients

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -37,21 +37,25 @@ class AppServiceProvider extends ServiceProvider
         ResetPassword::toMailUsing(function ($notifiable, $token) {
             return (new MailMessage)
                 ->subject('Resetowanie hasła')
+                ->greeting('Witaj!')
                 ->line('Otrzymujesz ten e-mail, ponieważ otrzymaliśmy prośbę o zresetowanie hasła dla Twojego konta.')
                 ->action('Resetuj hasło', url(config('app.url').route('password.reset', [
                     'token' => $token,
                     'email' => $notifiable->getEmailForPasswordReset(),
                 ], false)))
                 ->line('Ten link do resetowania hasła wygaśnie za :count minut.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')])
-                ->line('Jeśli nie prosiłeś o zresetowanie hasła, nie musisz podejmować żadnych dalszych działań.');
+                ->line('Jeśli nie prosiłeś o zresetowanie hasła, nie musisz podejmować żadnych dalszych działań.')
+                ->salutation("Pozdrawiamy,\nZespół ".config('app.name'));
         });
 
         VerifyEmail::toMailUsing(function ($notifiable, $url) {
             return (new MailMessage)
                 ->subject('Weryfikacja adresu e-mail')
+                ->greeting('Witaj!')
                 ->line('Kliknij poniższy przycisk, aby zweryfikować swój adres e-mail.')
                 ->action('Zweryfikuj adres e-mail', $url)
-                ->line('Jeśli nie tworzyłeś konta, nie musisz podejmować żadnych dalszych działań.');
+                ->line('Jeśli nie tworzyłeś konta, nie musisz podejmować żadnych dalszych działań.')
+                ->salutation("Pozdrawiamy,\nZespół ".config('app.name'));
         });
     }
 

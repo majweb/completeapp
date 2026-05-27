58a09dcad6080a136ae81484faf55ee2d40a2b89
marcin <marcin.five@gmail.com>
2026-05-27 08:38:02 +0200

    clients

--- vite.config.ts
+++ vite.config.ts
@@ -27,5 +27,19 @@ export default defineConfig({
         wayfinder({
             formVariants: true,
         }),
+
     ],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                },
+            },
+        },
+        chunkSizeWarningLimit: 2700,
+        ssrManifest: true,
+    },
 });
